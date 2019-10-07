5f861875a874fcfa3d61ca244c92134c59e3e85a
Mertcan Isık <mertcanm061741@windowslive.com>
2019-10-07 16:36:35 +0300

    ilk commitim

---
+++ Odev1.java
@@ -0,0 +1,23 @@
+
+import java.util.Scanner;
+public class Odev1 {
+
+    
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int num1,num2;
+        System.out.print("ilk sayıyı giriniz:");
+        num1=scan.nextInt();
+        System.out.print("ikinci sayıyı giriniz:");
+        num2=scan.nextInt();
+        
+        num1=num1^num2;
+        num2=num2^num1;
+        num1=num1^num2;
+        
+        System.out.println("ilk sayi:"+num1);
+        System.out.println("ikinci sayi:"+num2);
+        
+    }
+    
+}
